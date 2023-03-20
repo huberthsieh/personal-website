d5e81455da6c1711cb88a935e36c6e4d7b85b39a
Hubert Hsieh <hubert780225@gmail.com>
2023-03-20 15:51:33 +0800

    feat/新增 新增Google font統一跨平台字型樣式

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,5 +47,22 @@ module.exports = {
                 ],
             },
         },
+        {
+            resolve: `gatsby-omni-font-loader`,
+            options: {
+                enableListener: true,
+                preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+                web: [
+                    {
+                        name: `Open Sans`,
+                        file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+                    },
+                    {
+                        name: `Noto Sans TC`,
+                        file: `https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500&display=swap`,
+                    },
+                ],
+            },
+        },
     ]
 };
